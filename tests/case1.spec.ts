55d199f83d6cb615061e7c5c3c5ed34b17fa6dbf
RoryC <Kar98@users.noreply.github.com>
2023-08-31 14:27:30 +1000

    added extra comments

--- tests/case1.spec.ts
+++ tests/case1.spec.ts
@@ -1,5 +1,13 @@
 import { test, expect } from '@playwright/test';
 
+/** Front end labels (same order as backend data) : 
+ * 
+ * Some value
+ * A really long name
+ * Standard
+ * Short
+ * Another value
+ */
 /** API doc spec */
 const BE_DATA = {
   data: [
@@ -33,13 +41,19 @@ class Api{
   }
 }
 
-test('Title conversion', async () => {
+test('Title conversion', async ({page}) => {
+  /**
+   * Originally it was thought that the data would be in the same order everytime and the below test was written. However when the API
+   * was implemented, it was found that the data varies.
+   * 
+   * Task : Write a test that will check all backend labels match the expected front end label. 
+   */
   let json = await Api.get_value();
   console.log(json);
   expect(json.data[0].name).toEqual('Some value');
   expect(json.data[1].name).toEqual('A really long name');
   expect(json.data[2].name).toEqual('Standard');
   expect(json.data[3].name).toEqual('Short');
-  expect(json.data[4].name).toEqual('Another type');
+  expect(json.data[4].name).toEqual('Another value');
 });
 
