41ffa21db07c7cb5d64df6356ff46b8ad5a49081
RoryC <Kar98@users.noreply.github.com>
2023-10-19 16:20:52 +1100

    working

---
+++ data/case4/index.js
@@ -0,0 +1,62 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const hostname = "localhost";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log("Request for " + req.url + " by method " + req.method);
+
+  if (req.method == "GET") {
+    var fileUrl;
+    if (req.url == "/") {
+      fileUrl = "./index.html";
+    } else {
+      fileUrl = req.url;
+    }
+
+    var filePath = path.resolve("./data/case4/" + fileUrl);
+    const fileExt = path.extname(filePath);
+    console.log(`Filepath : ${filePath}`);
+    if (fileExt == ".html") {
+      fs.exists(filePath, (exists) => {
+        if (!exists) {
+          filePath = path.resolve("./404.html");
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/html");
+          fs.createReadStream(filePath).pipe(res);
+          return;
+        }
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        fs.createReadStream(filePath).pipe(res);
+      });
+    } else if (fileExt == ".css") {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/css");
+      fs.createReadStream(filePath).pipe(res);
+    } else if (fileExt == ".jpg") {
+      if (fs.existsSync(filePath)) {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "image/jpeg");
+        fs.createReadStream(filePath).pipe(res);
+      }
+    } else {
+      console.log("missed");
+      filePath = path.resolve("./data/case4/404.html");
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html");
+      fs.createReadStream(filePath).pipe(res);
+    }
+  } else {
+    filePath = path.resolve("./404.html");
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    fs.createReadStream(filePath).pipe(res);
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
